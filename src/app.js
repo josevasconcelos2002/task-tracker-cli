e7bfab281fa10ae764ec9f2dea5c5742e5ad7eb9
Jose Vasconcelos <zevasconcelos6@gmail.com>
2025-10-30 10:23:53 +0000

    feat: parsing json, saving feature and delete

--- src/app.js
+++ src/app.js
@@ -6,81 +6,111 @@ const homeDir = os.homedir();
 const filePath = path.join(homeDir, "task-tracker-cli", "data", "tasks.json");
 let lastId = 0;
 
-async function main(){
+let data = {
+    tasks: {
+        todo: [],
+        "in-progress": [],
+        done: []
+    }
+};
+
+async function main() {
     const args = process.argv.slice(2);
 
-    if(args.length == 0){
+    if (args.length == 0) {
         console.log("0 arguments provided!");
         help();
+        return;
     }
 
-    fs.stat(filePath, (err, stat) => {
+    try {
+        if (fs.existsSync(filePath)) {
+            console.log(filePath + " file already exists!");
+            const content = fs.readFileSync(filePath, "utf8").trim();
 
-        if(!err){
-            console.log( filePath + " file already exists!");
-        }
-
-        else if(err.code === 'ENOENT'){
+            if (content.length > 0) {
+                data = JSON.parse(content);
+                lastId = Math.max(
+                    0,
+                    ...data.tasks["todo"].map(t => t.id),
+                    ...data.tasks["in-progress"].map(t => t.id),
+                    ...data.tasks["done"].map(t => t.id)
+                );
+            }
+        } else {
             console.log("Creating tasks.json file ...");
-            const fd = fs.openSync(filePath, 'w');
-            fs.closeSync(fd);
+            fs.mkdirSync(path.dirname(filePath), { recursive: true });
+            fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
         }
-    })
+    } catch (err) {
+        console.error("Erro ao ler ficheiro:", err);
+        return;
+    }
 
     const operationType = args[0];
-    switch(operationType){
+
+    switch (operationType) {
         case "add":
-            if(args.length != 2){
-                console.log("Missing Task description!!");
+            if(args.length != 2) {
+                console.log("Missing Task <description>!");
                 break;
             }
             addTask(args[1]);
             break;
         
-        
+        case "delete":
+            if(args.length != 2){
+                console.log("Missing Task <id>!");
+                break;
+            }
+            deleteTask(args[1]);
+            break;
     }
+
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+    console.log("tasks.json saved!");
 }
 
-function addTask(description){
 
-    let data = {
-        tasks: {
-        todo: [],
-        "in-progress": [],
-        done: []
-        }
-    };
-
-    if (fs.existsSync(filePath)) {
-        const content = fs.readFileSync(filePath, "utf8").trim();
-        if (content.length > 0) {
-        try {
-            data = JSON.parse(content);
-        } catch (err) {
-            console.error("Erro a fazer parsing do JSON:", err);
-            return;
-        }
-        }
-    }
+function addTask(description) {
 
     const now = new Date().toISOString();
     lastId++;
 
     const newObject = {
         id: lastId,
-        description,
+        description: description,
         status: "todo",
         createdAt: now,
         updatedAt: now
     };
 
     data.tasks.todo.push(newObject);
-    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+    console.log("Task: " + newObject.id + " -> " + newObject.description + " successfully added!");
+}
+
+function deleteTask(id){
+
+    for(const status of ["todo", "in-progress", "done"]){
+        const previousLength = data.tasks[status].length;
+        data.tasks[status] = data.tasks[status].filter((entry) => {
+            if(entry.id != id)
+                return entry;
+        });
+        const newLength = data.tasks[status].length;
+
+        if(newLength == previousLength){
+            console.log("Task " + id + " doesnt exist in " + status);
+        } else{
+            console.log("Task " + id + " deleted in " + status);
+        }
+
+    }
 }
 
 
 
-function help(){
+function help() {
     const addHelp = "Add feature:  add <description>\n\n";
     const updateHelp = "Update feature:  update <id> <newDescription>\n\n";
     const deleteHelp = "Delete feature:  delete <id>\n\n";
